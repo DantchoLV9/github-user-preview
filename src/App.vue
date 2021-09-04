b082e54d077964463f3f6b3e2ede754065383305
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2021-09-04 21:49:18 +0100

    Project Init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Hello, World!</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
